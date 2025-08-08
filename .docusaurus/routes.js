b52f0e5dce511b649004c48bbe153f6975a7c374
truongpham2309 <phamnhattruong2309@gmail.com>
2025-08-08 14:00:26 +0700

    Add .gitignore and untrack node_modules

---
+++ .docusaurus/routes.js
@@ -0,0 +1,200 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/',
+    component: ComponentCreator('/', '725'),
+    routes: [
+      {
+        path: '/',
+        component: ComponentCreator('/', 'af4'),
+        routes: [
+          {
+            path: '/',
+            component: ComponentCreator('/', 'd99'),
+            routes: [
+              {
+                path: '/phan-he-don-tiep/canh-bao-khi-tiep-don',
+                component: ComponentCreator('/phan-he-don-tiep/canh-bao-khi-tiep-don', 'bf6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/chinh-sua-phong-kham',
+                component: ComponentCreator('/phan-he-don-tiep/chinh-sua-phong-kham', 'a12'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/chinh-sua-thong-tin-hanh-chinh',
+                component: ComponentCreator('/phan-he-don-tiep/chinh-sua-thong-tin-hanh-chinh', 'a40'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/giao-dien-va-dang-nhap',
+                component: ComponentCreator('/phan-he-don-tiep/giao-dien-va-dang-nhap', 'b10'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/in-lai-phieu-kham',
+                component: ComponentCreator('/phan-he-don-tiep/in-lai-phieu-kham', 'ef8'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/nhap-cac-thong-tin-lien-quan',
+                component: ComponentCreator('/phan-he-don-tiep/nhap-cac-thong-tin-lien-quan', '42e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/tiep-don-benh-nhan-bhyt',
+                component: ComponentCreator('/phan-he-don-tiep/tiep-don-benh-nhan-bhyt', '646'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/tiep-don-benh-nhan-khong-co-the-bhyt',
+                component: ComponentCreator('/phan-he-don-tiep/tiep-don-benh-nhan-khong-co-the-bhyt', '9ee'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/xem-tinh-trang-cac-phong-kham',
+                component: ComponentCreator('/phan-he-don-tiep/xem-tinh-trang-cac-phong-kham', '0a6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-don-tiep/y-nghia-chuc-nang-phan-mem',
+                component: ComponentCreator('/phan-he-don-tiep/y-nghia-chuc-nang-phan-mem', '967'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-duoc-vat-tu/dang-nhap-chon-phong',
+                component: ComponentCreator('/phan-he-duoc-vat-tu/dang-nhap-chon-phong', '123'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-noi-tru/dang-nhap-dang-xuat-doi-mat-khau',
+                component: ComponentCreator('/phan-he-noi-tru/dang-nhap-dang-xuat-doi-mat-khau', 'a33'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-phong-kham/dang-nhap-chon-phong-kham',
+                component: ComponentCreator('/phan-he-phong-kham/dang-nhap-chon-phong-kham', '3a2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/bao-cao',
+                component: ComponentCreator('/phan-he-thu-ngan/bao-cao', 'dce'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/cac-thao-tac-thu-ngan',
+                component: ComponentCreator('/phan-he-thu-ngan/cac-thao-tac-thu-ngan', 'e34'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/cac-truong-loc-tim-kiem-bn-quet-ma-qrcode',
+                component: ComponentCreator('/phan-he-thu-ngan/cac-truong-loc-tim-kiem-bn-quet-ma-qrcode', '367'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/dang-nhap-chon-phong-thu-ngan',
+                component: ComponentCreator('/phan-he-thu-ngan/dang-nhap-chon-phong-thu-ngan', '73b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/duyet-khoa-BH',
+                component: ComponentCreator('/phan-he-thu-ngan/duyet-khoa-BH', 'bb6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/gioi-thieu-chung-mang-hinh-thu-ngan',
+                component: ComponentCreator('/phan-he-thu-ngan/gioi-thieu-chung-mang-hinh-thu-ngan', '8d5'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/huy-giao-dich-in-phieu-tuong-ung',
+                component: ComponentCreator('/phan-he-thu-ngan/huy-giao-dich-in-phieu-tuong-ung', '32c'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/khoa-vien-phi',
+                component: ComponentCreator('/phan-he-thu-ngan/khoa-vien-phi', 'b50'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/kiem-tra-lich-su-giao-dich',
+                component: ComponentCreator('/phan-he-thu-ngan/kiem-tra-lich-su-giao-dich', 'c06'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/mien-giam-khi-thanh-toan-tien',
+                component: ComponentCreator('/phan-he-thu-ngan/mien-giam-khi-thanh-toan-tien', 'd91'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/tao-so-bien-lai-hoa-don',
+                component: ComponentCreator('/phan-he-thu-ngan/tao-so-bien-lai-hoa-don', 'a7d'),
+                exact: true
+              },
+              {
+                path: '/phan-he-thu-ngan/thao-tac-khoa-mo-bh-vien-phi',
+                component: ComponentCreator('/phan-he-thu-ngan/thao-tac-khoa-mo-bh-vien-phi', 'bce'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/xem-bang-ke-chi-phi',
+                component: ComponentCreator('/phan-he-thu-ngan/xem-bang-ke-chi-phi', '723'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/y-nghia-cac-mau-tai-man-hinh-vien-phi-duyet-bhyt',
+                component: ComponentCreator('/phan-he-thu-ngan/y-nghia-cac-mau-tai-man-hinh-vien-phi-duyet-bhyt', '3bc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/phan-he-thu-ngan/y-nghia-cac-thong-tin-tai-man-hinh-chi-tiet-vien-phi',
+                component: ComponentCreator('/phan-he-thu-ngan/y-nghia-cac-thong-tin-tai-man-hinh-chi-tiet-vien-phi', 'edc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/',
+                component: ComponentCreator('/', 'fc9'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
